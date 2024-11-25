64e75319077aeced623087f65c75e3db0da7288d
Bob Olde Hampsink <bob@robuust.digital>
2024-11-25 14:22:04 +0100

    Supports webhooks

--- src/gateways/Gateway.php
+++ src/gateways/Gateway.php
@@ -58,6 +58,14 @@ class Gateway extends OffsiteGateway
     /**
      * {@inheritdoc}
      */
+    public function supportsWebhooks(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function rules(): array
     {
         $rules = parent::rules();
